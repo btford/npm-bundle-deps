12c4e734835367e49c3f1efda2c69ccc5904c237
Brian Ford <btford@umich.edu>
2014-06-30 11:22:04 -0700

    wip: refactor

--- app.js
+++ app.js
@@ -2,18 +2,32 @@
 
 var http      = require('http');
 var Router    = require('routes-router');
-var sendFile  = require('./send-file.js');
-var fetchTar  = require('./fetch-tar.js');
+var sendFile  = require('./send-file');
+var fetchTar  = require('./fetch-tar');
+var Cache     = require('./lib/cache');
+var log       = require('./lib/log');
 
-var app = Router();
+var argv  = require('minimist')(process.argv.slice(2));
+var app   = Router();
+var cache = Cache();
+
+log.set(!!argv.log);
+log.prefix(function () {
+  return new Date().toString() + ': ';
+});
 
 app.addRoute('/:user/:repo/:sha', function (req, res, opts, cb) {
   var repo = opts.user + '/' + opts.repo;
   var sha = opts.sha;
+  log('requested ' + repo + ' @ ' + sha);
   fetchTar(repo, sha, function (path) {
+    cache.set(path);
+    log('sending ' + path);
     sendFile(req, res, path, cb);
   });
 });
 
+var port = argv.port || 3000;
 var server = http.createServer(app);
-server.listen(3000);
+server.listen(port);
+log('listening on ' + port);
--- fetch-tar.js
+++ fetch-tar.js
@@ -1,27 +1,29 @@
-var cp       = require('child_process');
-var fs       = require('fs');
-var path     = require('path');
-var mkdirp   = require('mkdirp');
-var request  = require('request');
-var checksum = require('checksum');
+var exec       = require('child_process').exec;
+var fs         = require('fs');
+var path       = require('path');
+var mkdirp     = require('mkdirp');
+var checksum   = require('checksum');
+var request    = require('request');
+var log        = require('./lib/log');
+var npmInstall = require('./lib/npm-install');
+
+var PACKAGE_FILES = [
+  'package.json',
+  'npm-shrinkwrap.json'
+];
 
 module.exports = function (repo, sha, cb) {
   var npmDir = path.join('./temp', repo, sha);
 
-  var packageFiles = [
-    'package.json',
-    'npm-shrinkwrap.json'
-  ];
-
-  var packageJsonPath = path.join(npmDir, 'package.json');
+  var packageJsonPath    = path.join(npmDir, 'package.json');
   var shrinkwrapJsonPath = path.join(npmDir, 'npm-shrinkwrap.json');
 
   function fetchPackageJsonFile (cb) {
     mkdirp.sync(npmDir);
 
-    packageFiles.forEach(function (file) {
+    PACKAGE_FILES.forEach(function (file) {
       var url = githubFileUrl(repo, sha, file);
-      console.log(url);
+      log('requesting ' + url);
       request(url).
           pipe(fs.createWriteStream(path.join(npmDir, file))).
           on('finish', V);
@@ -30,17 +32,14 @@ module.exports = function (repo, sha, cb) {
     // i regret nothing
     var semaphore = 2;
     function V (err, data) {
-      --semaphore && cb(err, data);
+      --semaphore && (log('got both files'), cb(err, data));
     }
   }
 
-  function npmInstall (cb) {
-    cp.exec('npm install', { cwd: npmDir }, cb);
-  }
 
   function tarNodeModules (outputFile, cb) {
     var cmd = ['tar -zcf', '../../../../' + outputFile, 'node_modules'].join(' ');
-    cp.exec(cmd,
+    exec(cmd,
         { cwd: npmDir },
         function (err, stdout) {
           cb(outputFile);
@@ -51,14 +50,23 @@ module.exports = function (repo, sha, cb) {
     var fileToCheck = fs.existsSync(shrinkwrapJsonPath) ?
         shrinkwrapJsonPath : packageJsonPath;
 
+    log('using checksum from ' + fileToCheck);
+
     checksum.file(fileToCheck, function (err, sum) {
+      log(fileToCheck + ' -> ' + sum);
       var outputPath = path.join('./files', repo, sum);
       var outputFile = path.join(outputPath, 'node_modules.tar.gz');
+      log('checking for ' + outputFile);
       if (fs.existsSync(outputFile)) {
-        cb(outputFile)
+        log(outputFile + ' exists on disk');
+        cb(outputFile);
       } else {
+        log(outputFile + ' does not exist on disk');
         mkdirp.sync(outputPath);
-        npmInstall(function () {
+
+        log('running npm install in ' + npmDir);
+        npmInstall(npmDir, function (err, data) {
+          log(err ? 'npm install finished with an error code' : 'npm install finished');
           tarNodeModules(outputFile, cb);
         });
       }
@@ -67,8 +75,10 @@ module.exports = function (repo, sha, cb) {
 
 
   if (fs.existsSync(packageJsonPath)) {
+    log('`package.json` is cached');
     getTarForPackageJson(cb);
   } else {
+    log('`package.json` is not cached');
     fetchPackageJsonFile(function () {
       getTarForPackageJson(cb);
     });
